4abc2e9159cb5f4228acf5506e4de4b905140e5d
kchoi123 <k3vinch0i@gmail.com>
2019-02-27 15:28:03 -0800

    edits to js functions and button clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,6 @@
 var compGuess = 0;
 var wins = 0;
 var losses = 0;
-var userNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var img1;
 var img2;
 var img3;
@@ -21,36 +20,30 @@ var maxUser = 12;
 
 // ------------------- All functions below -------------------
 
-// create random number gen from 19 - 120
-function compGen() {
-    do {
-        randomGenerator();
-    }
-    while (compGuess < 19);
-    $(".compGuess-text").text(compGuess);
-    console.log("comp generated number: " + compGuess);
-};
-
-// write own random function to not be 0
-// fix while statement
-// create random number gen from 1-12
-function randomGenerator() {
+// comp generator
+function randomCompGenerator() {
     compGuess = Math.ceil(Math.random() * (maxComp - minComp + 1));
 }
 
-function userGen() {
-    for (var i = 0; i < 4; i++) {
-
+// create random number gen from 19 - 120
+function compGen() {
+    do {
+        randomCompGenerator();
     }
-    
-    // do {
-    //     var interval = maxUser - minUser + 1;
-    //     img1 = Math.floor(Math.random() * (maxUser - minUser + 1));
-    //     img2 = Math.floor(Math.random() * (maxUser - minUser + 1));
-    //     img3 = Math.floor(Math.random() * (maxUser - minUser + 1));
-    //     img4 = Math.floor(Math.random() * (maxUser - minUser + 1));
-    // }
-    // while ((img1 && img2 !== img3 && img4) && (img2 && img3 !== img1 && img4) /* && (img1, img2, img3, img4 !== 0) */);
+    while (compGuess < 19);
+    $(".compGuess-text").text(compGuess);
+    // console.log("comp generated number: " + compGuess);
+};
+
+// create random number gen from 1-12
+function userGen() {    
+    do {
+        img1 = Math.ceil(Math.random() * (maxUser - minUser + 1));
+        img2 = Math.ceil(Math.random() * (maxUser - minUser + 1));
+        img3 = Math.ceil(Math.random() * (maxUser - minUser + 1));
+        img4 = Math.ceil(Math.random() * (maxUser - minUser + 1));
+    }
+    while (img1 !== img2, img2 !== img3, img3 !== img4, img4 !== img1);
     // console.log(img1 + " " + img2 + " " + img3 + " " + img4);
 };
 
@@ -60,13 +53,13 @@ function winLoss() {
         console.log("you win")
         wins++;
         //display wins + 1
-        $("#win-text").text(wins);
+        $("#win-text").text("Wins: " + wins);
         reset();
     } else if (userTotal > compGuess) {
         console.log("you loser")
         losses++;
         //display losses + 1
-        $("#losses-text").text(losses);
+        $("#losses-text").text("Losses: " + losses);
         reset();
     }
 };
@@ -88,7 +81,7 @@ userGen();
 // click function for image 1
 $(".img1").on("click", function () {
     userTotal += img1;
-    console.log(userTotal);
+    // console.log(userTotal);
     //Your total score
     $(".userTotal").text(userTotal);
     //calling the reset when the user wins
@@ -98,7 +91,7 @@ $(".img1").on("click", function () {
 // click function for image 2
 $(".img2").on("click", function () {
     userTotal += img2;
-    console.log(userTotal);
+    // console.log(userTotal);
     //Your total score
     $(".userTotal").text(userTotal);
     //calling the reset when the user wins
@@ -108,7 +101,7 @@ $(".img2").on("click", function () {
 // click function for image 3
 $(".img3").on("click", function () {
     userTotal += img3;
-    console.log(userTotal);
+    // console.log(userTotal);
     //Your total score
     $(".userTotal").text(userTotal);
     //calling the reset when the user wins
@@ -118,9 +111,9 @@ $(".img3").on("click", function () {
 // click function for image 4
 $(".img4").on("click", function () {
     userTotal += img4;
-    console.log(userTotal);
+    // console.log(userTotal);
     //Your total score
-    $(".userTotal-text").text(userTotal);
+    $(".userTotal").text(userTotal);
     //calling the reset when the user wins
     winLoss();
 });
